e98fe14bcc24e2518b661759ad25601af8957ed9
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-12-09 15:17:19 +0100

    where we start from + installed bootstrapvue

---
+++ src/components/Chat.vue
@@ -0,0 +1,17 @@
+<template>
+<h2>Example heading <b-badge>New</b-badge></h2>
+</template>
+
+<script>
+export default {
+  name: 'Chat',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
