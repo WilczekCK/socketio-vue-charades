aeffa3e9b12617417cc9ab88f89187c758b9b153
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-12-15 14:43:21 +0100

    rebuilded the modal at the beginning

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <template>
   <div id="app">
-    <b-modal ref="nickname-modal" id="modal-scrollable" scrollable no-close-on-esc no-close-on-backdrop title="Please, provide your username">
-      <h6>Please, provide your username!</h6>
-      <b-form-input v-model="username" placeholder="You will be displayed as a Guest"></b-form-input>
+    <b-modal 
+      ref="nickname-modal"
+      id="modal-scrollable"
+      no-close-on-esc
+      no-close-on-backdrop 
+      hide-header-close 
+      hide-footer
+      title="Please, provide your username to start the game">
+        <b-form-input
+          placeholder="You will be displayed as a Guest"
+          v-model="loginData.username">
+        </b-form-input>
+
+        <b-button 
+          @click="finishedWriting" 
+          variant="primary"
+          block>
+          I want to play! 
+        </b-button>
     </b-modal>
 
     <b-container class="game__container">
@@ -10,7 +26,7 @@
     </b-container>
 
     <b-container class="chat__container">
-      <Chat :username="username" />
+      <Chat :loginData="loginData" />
     </b-container>
   </div>
 </template>
@@ -23,15 +39,24 @@ export default {
   name: 'app',
   data () {
     return {
-      username: 'Guest'
+      loginData: {
+        username: 'Guest',
+        isWritingFinished: false,
+      }
     }
   },
   components: {
     Chat,
     Game
   },
+  methods:{
+    finishedWriting: function() {
+      this.$refs['nickname-modal'].hide();
+      this.loginData.isWritingFinished = true;
+    }
+  },
   mounted() {
-    this.$refs['nickname-modal'].show()
+    this.$refs['nickname-modal'].show();
   }
 }
 </script>
--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -3,10 +3,10 @@
         <b-container class="chat__menu">
             <b-row>
                 <b-col>
-                      <b-button block variant="primary">Change nickname</b-button>
+                      <b-button block variant="primary">I want draw</b-button>
                 </b-col>
                 <b-col>
-                      <b-button block variant="primary">Ask for help</b-button>
+                      <b-button block variant="primary">Ask for tip</b-button>
                 </b-col>
                 <b-col>
                       <b-button block variant="primary">FAQ</b-button>
@@ -15,7 +15,7 @@
         </b-container>
 
         <b-container fluid class="chat__system">
-            <b-table striped sticky-header small :items="messages" style="max-height:130px;"></b-table>
+            <b-table striped sticky-header small :items="messages"></b-table>
             <b-row>
                 <b-col cols="12">
                 <b-form-input v-model="message" placeholder="Enter your message" @keyup.enter="sendMessage()"></b-form-input>
@@ -34,12 +34,13 @@
 import io from  'socket.io-client';
 export default {
   name: 'Chat',
-  props: ['username'],
+  props: ['loginData'],
   data() {
       return {
-        user: this.username,
+        user: 'guest',//this.username,
         message: '',
         messages: [],
+        loginCred: this.loginData,
         socket: io('http://localhost:3001')
       }
   },  
@@ -59,8 +60,12 @@ export default {
       })
   },
   watch: {
-      username: function(newVal){
-          this.user = newVal ? newVal : 'Guest';
+      loginCred: function(newVal){
+          console.log(newVal)
+        if(newVal.isWritingFinished){
+            this.user = newVal.username ? newVal.username : 'Guest';
+            this.messages = [...this.messages, {user:'System', message:`Hello ${this.user}, there is 0 players online`} ];
+        }
       }
   }
 }
