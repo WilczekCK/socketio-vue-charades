68ec214e64050a4fdc786b3aea6358001c7983a3
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-12-15 16:08:07 +0100

    playerlist fully functional

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const playerList = [];
 
 server = app.listen(3001, function(){
     console.log('server is running on port 3001')
@@ -12,10 +13,23 @@ const io = require('socket.io')(server, {
     }
 });
 
+
 io.on('connection', function(socket){
+    console.log(`User ${socket.id} connected`)
     let actualConnected = socket.id;
+    playerList.push(actualConnected);
 
     socket.on("SEND_MESSAGE", function(data){
         io.emit("MESSAGE", data);
+    })
+
+
+    socket.on('disconnect', () => {
+        console.log(`User ${socket.id} disconnected`)
+
+        //Remove user from the array
+        playerList = playerList.filter(function(player){
+            return player.id !== socket.id
+        })
     })
 })
