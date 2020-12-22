bcd3379f5f32781f5597731fa3357bfe37fe7e44
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-12-22 13:32:31 +0100

    basic roll player and init schema of drawing player function

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -5,10 +5,47 @@
 </template>
 
 <script>
+//import _ from 'underscore';
 export default {
   name: 'Game',
-  props: {
-    msg: String
+  props: ['socket', 'playerList'],
+  data(){
+    return {
+      isDrawing: false,
+      timeForDrawing: 120, //seconds
+      drawingPlayer: {
+        username: undefined,
+        id: null,
+        points: null
+      }
+    }
+  },
+  methods: {
+    getRandomPlayer(obj){
+      let keys = Object.keys(obj);
+      return obj[keys[ keys.length * Math.random() << 0]];
+    },
+    selectDrawingPlayer(){
+      this.drawingPlayer = this.getRandomPlayer(this.playerList);
+
+      this.socket.emit('SEND_MESSAGE', {
+        username: "GAME",
+        message: `Player which will draw is: ${this.drawingPlayer.username}, wait till he select the word!`,
+        type: 'system__message'
+      });
+      
+      this.isDrawing = true;
+    }
+  },
+  mounted: function(){
+
+  },
+  watch:{
+    playerList: function(){
+      if(this.playerList.length > 1 && !this.isDrawing){
+        this.selectDrawingPlayer();
+      }
+    }
   }
 }
 </script>
