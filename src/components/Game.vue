07ac1e66765d9a39c04c00036956cc26b9ae40fd
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-01-11 15:21:45 +0100

    different shapes working, yay!

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,23 +1,60 @@
 <template>
 <div style="height:100%;">
-    <v-stage ref="blackboard" :config="conva.config" @mousemove="draw" @mousedown="startDraw"  @mouseup="stopDraw">
+    <v-stage ref="blackboard" :config="conva.config" @mousemove="draw(gameData.actualBrush)" @mousedown="startDraw"  @mouseup="stopDraw">
       <v-layer>
         <v-rect :config="conva.rect"></v-rect>>
         
   
         <v-text v-if="gameData.wordSelected" :config="{text: `The word you have to draw is: ${ gameData.wordSelected }`, y: 50, x:6, fontSize: 12}"  />
        
+          <v-circle
+          v-for="circle in gameData.paintings.circles"
+          :key="circle.id"
+          :config="{
+              fill: circle.color,
+              x: circle.x,
+              y: circle.y,
+              radius: 70
+          }">
+          </v-circle>
 
-        <v-circle
-        v-for="paint in gameData.paintings"
-        :key="paint.id"
-        :config="{
-            fill: paint.color,
-            x: paint.x,
-            y: paint.y,
-            radius: 70
-        }">
-        </v-circle>
+          <v-rect
+          v-for="rect in gameData.paintings.rects"
+          :key="rect.id"
+          :config="{
+              fill: rect.color,
+              x: rect.x,
+              y: rect.y,
+              height:25,
+              width:25
+          }">
+          </v-rect>
+  
+          <v-star
+          v-for="star in gameData.paintings.stars"
+          :key="star.id"
+          :config="{
+              fill: star.color,
+              x: star.x,
+              y: star.y,
+              innerRadius: 8,
+              outerRadius: 16,
+          }">
+          </v-star>
+
+          <v-ring
+          v-for="ring in gameData.paintings.rings"
+          :key="ring.id"
+          :config="{
+              fill: ring.color,
+              x: ring.x,
+              y: ring.y,
+              innerRadius: 8,
+              outerRadius: 16,
+              numPoints: 6
+          }">
+          </v-ring>
+
 
 
         <headerLabel />
@@ -63,6 +100,8 @@
 import headerLabel from './canvas/headerLabel';
 import toolbox from './canvas/toolbox';
 import ColorPicker from '@radial-color-picker/vue-color-picker';
+//import _ from 'underscore';
+
 export default {
   name: 'Game',
   components: {headerLabel, toolbox, ColorPicker},
@@ -78,9 +117,17 @@ export default {
       gameData: {
         isMouseButtonHold: false,
         actualBrush: 'circle',
-        paintings: [
+        paintings: {
+          circles: [{
+            x: 103,
+            y: 200,
+            color: 'hsl(137.2025632945273, 100%, 50%)'
+          }],
+          rects:  [],
+          stars: [],
+          rings: [],
           //x, y, color
-        ],
+        },
         wordSelected: undefined,
         drawingPlayer: {
           username: undefined,
@@ -107,12 +154,12 @@ export default {
     setBrush(brush){
       this.gameData.actualBrush = brush;
     },
-    draw(){
+    draw(brush){
       if(!this.gameData.isMouseButtonHold) return;
       const mousePos = this.$refs.blackboard.getNode().getPointerPosition();
-
-      this.gameData.paintings.push({x:mousePos.x, y:mousePos.y, color: `hsl(${this.color.hue}, ${this.color.saturation}%, ${this.color.luminosity}%)`});
+      this.gameData.paintings[brush+'s'].push({x:mousePos.x, y:mousePos.y, color: `hsl(${this.color.hue}, ${this.color.saturation}%, ${this.color.luminosity}%)`});
     },
+     
     startDraw(){
       this.gameData.isMouseButtonHold = true;
     },
