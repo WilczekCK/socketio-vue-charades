0241944deb08f6779f57173ed5aaf0362a2f341b
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-01-19 14:58:28 +0100

    made an url which connects to random words api

--- server/controllers/wordsApi.js
+++ server/controllers/wordsApi.js
@@ -1,5 +1,9 @@
 const wordsApi = {
-
+    baseUrl: `https://api.datamue.com/words?&max=${this.maxResults}&rel_${this.partOfSpeech}`,
+    partOfSpeech: `jja`, //search for nouns - based on datamuse.com/api
+    maxResults: 25,
+    wordsRelatedTo: (word) => word ? word : `random`,
+    makeApiConnection: (word) => baseUrl + this.wordRelatedTo(word)
 }
 
 module.exports = wordsApi;
